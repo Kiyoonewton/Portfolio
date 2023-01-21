862b202d5509fc7ead20e2cd5b10ea5a5f54967a
kiyoonewton <kiyoonewton41@gmail.com>
2023-01-21 13:41:29 +0100

    Add some an Image

--- kiyoo-portfolio/pages/index.js
+++ kiyoo-portfolio/pages/index.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import Image from "next/image";
+import profile from "../public/work.jpg";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 export default function Home() {
   return (
@@ -12,7 +20,86 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className="bg-white px-10">
+        <section className=" min-h-screen ">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">CODEDEDBYKIYOO</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-500 text-medium ">
+              Kolawole Isaac
+            </h2>
+            <h3 className="text-3xl py-2 pt-0 text-teal-500 text-medium">
+              (Kiyoonewton)
+            </h3>
+            <h3 className="text-2xl py-2">Software Engineer</h3>
+            <p className="text-md py-5 text-gray-700 leading-8">
+              A developer providing services for Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Quas, nulla consequuntur. Nulla
+              vero quis distinctio eveniet alias, labore ipsa ipsum tenetur,
+              aspernatur suscipit nostrum illum!
+            </p>
+          </div>
+          <div className="text-5xl flex justify-between gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className="rounded-lg my-7">
+            <Image
+              className="rounded-3xl mx-auto bg-gradient-to-b from-gray-700"
+              src={profile}
+              width={300}
+              height={200}
+            />
+          </div>
+        </section>
+        <section>
+          <h3 className="text-3xl py-1">Services I offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-700">
+            Since I have begun my journey as a Software Engineer, I have done a
+            few works both working as a contractor and a freelancer also worked
+            for a few <span className="text-teal-500">agencies</span> and
+            consulted for a few <span className="text-teal-500">startups</span>.
+            I can assure you, you are in good hands.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-700 dark:text-gray-500">
+            I offer fro a large range of services, including programming and
+            sometimes create time for teaching{" "}
+          </p>
+          {/* </div> */}
+          <div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={design} width={100} height={100} />
+
+              <h3 className="text-lg font-medim pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                creating elegant designs suited for your needs following core
+                design theory
+              </p>
+              <p className="py-4 text-teal-600">Design tools I use</p>
+              <h4 className="text-gray-700 py-1">Photoshop</h4>
+              <h4 className="text-gray-700 py-1">Illustrator</h4>
+              <h4 className="text-gray-700 py-1">Figma</h4>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- kiyoo-portfolio/tailwind.config.js
+++ kiyoo-portfolio/tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        burtons: "Burtons",
+      },
+    },
   },
   plugins: [],
-}
+};
